bc7e6792c58a0dc04726d7d63ced9475b2249c88
tomsecret <bladelun@gmail.com>
2017-07-02 19:04:44 +0000

    heroku

--- http_example.js
+++ http_example.js
@@ -3,14 +3,21 @@ var https = require('https');
 console.log('I did it!');
 
 var options = {
-  host: 'www.example.org',
-  path: '/'
+  host: 'stream-large-file.herokuapp.com',
+  path: '/give-me-stuff-now'
 };
 
 // called by https when the request is made.
-var callback = function() {
+var callback = function(response) {
   console.log('In response handler callback!');
+
+  response.on('data', function(chunk) {
+    console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+    console.log(chunk.toString());
+  });
 }
+
+
 
 console.log("I'm about to make the request!");
 
